383f0ba3fc8b72e80ed92d873c7a92d466105469
Shrine Sabu <shrine02sabu@gmail.com>
2024-08-11 11:12:08 +0530

    admin panel

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,1 +1,31 @@
-# Register your models here.
+from django.contrib import admin
+from .models.hotel import Hotel
+from .models.reservation import Reservation
+from .models.room import Room
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ("name", "location", "is_active")
+    search_fields = ("name", "location")
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("hotel", "room_type", "luxury", "base_cost", "available_rooms")
+    list_filter = ("hotel", "room_type", "luxury")
+    search_fields = ("hotel__name", "room_type", "luxury")
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "room",
+        "check_in_date",
+        "number_of_days",
+        "total_cost",
+        "is_active",
+    )
+    list_filter = ("user", "room", "check_in_date")
+    search_fields = ("user__username", "room__hotel__name")
