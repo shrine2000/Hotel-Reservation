480c1b6b3662b7aa157c5d4b4c0bde73e50d3945
Shrine Sabu <shrine02sabu@gmail.com>
2024-08-24 12:32:11 +0530

    refactor from one to one to one to many in hotel model

--- reservations/models/hotel.py
+++ reservations/models/hotel.py
@@ -8,7 +8,7 @@ class Hotel(UIDModel, TimestampedModel):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     description = models.TextField()
-    admin = models.OneToOneField(User, on_delete=models.CASCADE)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name="hotels")
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
