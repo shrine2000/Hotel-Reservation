35133289d1c93b5949e9b6062c83aafad9e73f26
Shrine Sabu <shrine02sabu@gmail.com>
2024-08-24 08:55:10 +0530

    refactored room create endpoint

--- reservations/views/room_details.py
+++ reservations/views/room_details.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from reservations.models import Reservation
 from reservations.serializers import ReservationSerializer
@@ -11,8 +12,13 @@ class ReservationViewSet(viewsets.ModelViewSet):
     serializer_class = ReservationSerializer
     permission_classes = [IsAuthenticated]
 
-    def perform_create(self, serializer):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         room = serializer.validated_data["room"]
         if room.available_rooms < 1:
             raise serializers.ValidationError("No rooms available")
         serializer.save()
+        room.available_rooms -= 1
+        room.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
